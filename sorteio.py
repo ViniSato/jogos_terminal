b3bf38c0677e675d68489ad02a3e3662e5523839
ViniSato <103220549+ViniSato@users.noreply.github.com>
2022-04-15 21:39:46 -0300

    Add files via upload

---
+++ sorteio.py
@@ -0,0 +1,21 @@
+import random
+
+numero = round(random.randrange(1, 5))
+creme = 1
+leo = 2
+limao = 3
+sato = 4
+
+if(numero == creme):
+    print ("Creme")
+
+if(numero == leo):
+    print ("Léo")
+
+if(numero == limao):
+    print("Limão")
+
+if(numero == sato):
+    print("Sato")
+
+print()
